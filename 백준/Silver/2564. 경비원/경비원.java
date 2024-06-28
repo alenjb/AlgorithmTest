71ae3dc7c5f01ac9b25d2b28aba69af9ac036f41
이정빈 <alenjb@naver.com>
2024-06-28 21:18:02 +0900

    [Silver I] Title: 경비원, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Silver/2564. 경비원/경비원.java
+++ 백준/Silver/2564. 경비원/경비원.java
@@ -1,54 +1,43 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
-
+import java.util.*;
+ 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw =new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+ 
         int width = Integer.parseInt(st.nextToken());
-        int length = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        int num = Integer.parseInt(st.nextToken());
-        int [][] arr = new int[num][2];
-        for(int i=0; i<num; i++){
-            st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-        }
-        st = new StringTokenizer(br.readLine());
-        int dy = Integer.parseInt(st.nextToken());
-        int dx = Integer.parseInt(st.nextToken());
-        int answer = 0;
-        for(int i=0; i<num; i++){
-            int a = arr[i][0];
-            int b = arr[i][1];
-            //위치가 같으면
-            if(a == dy){
-                answer += Math.abs(b - dx);
-            }else if((Math.abs(a-dy)==1) && ((a + dy == 3) || (a + dy == 7))){
-                if(a + dy == 3) answer+= length + Math.min(2* width-dx-b, dx+b);
-                else answer+= width + Math.min(2*length-dx - b, dx+b);
-            }else if((dy==1 && a== 3) || (dy==3 && a== 1)){
-                answer+= dx + b;
-            }else if((dy==2 && a== 4) || (dy==4 && a== 2)){
-                answer+= length + width - dx - b;
-            }else if((dy==3 && a== 1) || (dy==4 && a== 2)){
-                answer+= length - dx +b;
-            }else if(dy==1 && a== 4){
-                answer+= width - dx +b;
-            }else if(dy==2 && a== 3){
-                answer+= length + dx -b;
-            }else if(dy==3 && a== 2){
-                answer+= length - dx +b;
-            }else if(dy==4 && a== 1){
-                answer+= width + dx -b;
+        int height = Integer.parseInt(st.nextToken());
+        int total = (width + height) << 1;
+ 
+        int N = Integer.parseInt(in.readLine());
+ 
+        //마지막 방은 동근이 위치
+        int[] spots = new int[N + 1];
+        for (int i = 0; i <= N; i++) {
+            st = new StringTokenizer(in.readLine());
+            int way = Integer.parseInt(st.nextToken());
+            spots[i] = Integer.parseInt(st.nextToken());
+ 
+            if (way == 2) spots[i] = width - spots[i];
+            else if (way == 3) spots[i] = height - spots[i];
+ 
+            switch (way) {
+                case 3: spots[i] += width;
+                case 2: spots[i] += height;
+                case 4: spots[i] += width;
             }
         }
-        bw.write(answer+"");
-        bw.flush();
+ 
+        //상점-동근위치 빼고, 그 길이와 토탈-길이 비교하기
+        int sum = 0;
+        for (int i = 0; i < N; i++) {
+            int len = Math.abs(spots[N] - spots[i]);
+            sum += Math.min(len, total - len);
+        }
+ 
+        System.out.println(sum);
     }
 }
