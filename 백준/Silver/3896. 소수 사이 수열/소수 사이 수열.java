beb4ed0f8fc5f9d4c3c4676bd90be09eacc1263f
이정빈 <alenjb@naver.com>
2024-07-15 17:31:50 +0900

    [Silver I] Title: 소수 사이 수열, Time: 1596 ms, Memory: 14800 KB -BaekjoonHub

---
+++ 백준/Silver/3896. 소수 사이 수열/소수 사이 수열.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n;
+	public static void main(String[] args) throws Exception{
+		// 1. 입력받기
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		n = Integer.parseInt(br.readLine());
+		for(int i=0; i<n; i++) {
+			int num = Integer.parseInt(br.readLine());			
+			//n이 소수인지 확인
+			if(isSosu(num)) bw.write("0\n");
+			else {
+				int small = 0;
+				int big = 0;
+				// 작은 소수 찾기
+				for(int j=num-1; j>1; j--) {
+					if(isSosu(j)) {
+						small = j;
+						break;
+					}
+				}
+				
+				// 큰 소수 찾기
+				for(int j=num+1; j<1299709; j++) {
+					if(isSosu(j)) {
+						big = j;
+						break;
+					}
+				}
+				bw.write((big-small)+"\n");
+			}
+		}
+		bw.write("\n");
+		bw.flush();
+	}
+	static boolean isSosu(int num) {
+		for(int i=2; i<num/2; i++) {
+			if(num % i == 0) return false;
+		}
+		return true;
+	}
+}
