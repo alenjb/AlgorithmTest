f5410376c30a9769212c199dab5018cb05a96a69
이정빈 <alenjb@naver.com>
2024-07-23 10:13:48 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 160 ms, Memory: 15896 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
@@ -8,20 +8,20 @@ public class Main {
 		// 입력받기
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		long num = Long.parseLong(br.readLine());
-		long result = num;
+		long num = Long.parseLong(br.readLine());	// 타겟 대상 수
+		long result = num;	//타겟 대상 수와 서로소인 개수
 		
 		
 		for(int i=2; i<=(int)Math.sqrt(num); i++) {
-			if(num % i == 0) { // 소인수이면
-				result = result - result / i;
-				while(num % i == 0) {
+			if(num % i == 0) { // 타겟 대상의 소인수이면
+				result = result - result / i; // 타겟 대상 수와 서로소인 개수 업데이트
+				while(num % i == 0) { // 타겟 대상에서 해당 소수를 모두 제거
 					num = num / i;
 				}
 			}
 		}
 		
-		// 자기 자신도 나눠주기
+		// num이 마지막 소인수인 경우 
 		if(num > 1) result = result - result /num;
 
 		bw.write(result+"\n");
