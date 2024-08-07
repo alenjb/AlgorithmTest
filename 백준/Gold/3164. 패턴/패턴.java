b3776140451c4cd7a609ba2f7e854f1e1a7d1547
이정빈 <alenjb@naver.com>
2024-08-07 17:24:05 +0900

    [Gold IV] Title: 패턴, Time: 160 ms, Memory: 18304 KB -BaekjoonHub

--- 백준/Gold/3164. 패턴/패턴.java
+++ 백준/Gold/3164. 패턴/패턴.java
@@ -1,59 +1,56 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int x1 = sc.nextInt();
-		int y1 = sc.nextInt();
-		int x2 = sc.nextInt()-1;
-		int y2 = sc.nextInt()-1;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine()); //한 줄 읽ㄱ기
+		int x1 = Integer.parseInt(st.nextToken());
+		int y1 = Integer.parseInt(st.nextToken());
+		int x2 = Integer.parseInt(st.nextToken()) -1;
+		int y2 = Integer.parseInt(st.nextToken()) -1;
+		
+		int min = Math.min(x1, y1);
+		int max = Math.max(x2, y2);
 		
 		long result = 0;
-		for(int i= x1; i<=x2; i++) {
-			if(i % 2 ==1) { //검은 색이면
-//				//세로
-//				System.out.printf("i : %d y1 %d\n",i , y1);
-				int column;
-				if(i >= y1) { //
-					int tmp = Math.min(i, y2);
-					column = tmp - y1 +1;	
-				}else {
-					column = 0;										
-				}
 
+		for(int i=min; i<=max; i++) {
+			if(i % 2 == 1) {
+				int column = 0;
+				int row = 0;
 				
-				result = result += column;			
-			}
-		}
-//		System.out.println("============="+result + "==========================");
-		
-		for(int i= y1; i<=y2; i++) {
-			if(i % 2 ==1) {
-				int row;
-				if(i >= x1) { //
-//					System.out.printf("i : %d x1 %d\n",i , x1);
-					int tmp = Math.min(i, x2);
-					row = tmp - x1 +1;		
-				}else {
-					row = 0;									
+				if(i>= x1 && i<= x2) {
+					
+					if(i >= y1) { //
+						int tmp = Math.min(i, y2);
+						if(tmp>0) column = tmp - y1 +1;	
+					}else {
+						column = 0;										
+					}					
 				}
-				result = result += row;							
+				if(i>= y1 && i<= y2) {				
+					if(i >= x1) {
+						int tmp = Math.min(i, x2);
+						if(tmp>0) row = tmp - x1 +1;			
+					}else {
+						row = 0;									
+					}
+					
+					if(row >0 && column>0) result--;
+				}
+				result = result + row + column;							
+
+
 			}
 		}
-		
-		// 중복 제거
-		int a = Math.max(x1, y1);
-		int b = Math.min(x2, y2);
-		for(int i=a; i<=b; i++) {
-			if(i% 2==1) {
-//			System.out.println(i + "를 뻄");
-//				System.out.println();
-				result --;
-				
-			}
-		}
-		
-		System.out.println(result);
-		
+		bw.write(result+"\n");
+		bw.flush();
+		bw.close();
 	}
+
 }
