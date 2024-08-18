78f880e3f8f93e07ce7af98333be833a212a9f94
이정빈 <alenjb@naver.com>
2024-08-18 21:22:28 +0900

    [Silver I] Title: 회전초밥, Time: 636 ms, Memory: 73444 KB -BaekjoonHub

--- 백준/Silver/28107. 회전초밥/회전초밥.java
+++ 백준/Silver/28107. 회전초밥/회전초밥.java
@@ -1,43 +1,55 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        LinkedList<Integer>[] sushi; // 초밥 배열
-        int[] eat; // 먹은 초밥 수 배열
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); // 손님 수
-        int M = sc.nextInt(); // 초밥 수
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        // 손님 수와 초밥 수 입력받기
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); // 손님 수
+        int M = Integer.parseInt(st.nextToken()); // 초밥 수
 
         // 손님별 먹은 초밥 수 배열 초기화
-        eat = new int[N + 1];
+        int[] eat = new int[N + 1];
         // 초밥 리스트 배열 초기화
-        sushi = new LinkedList[200_001];
+        LinkedList<Integer>[] sushi = new LinkedList[200_001];
         for (int i = 1; i <= 200_000; i++) {
             sushi[i] = new LinkedList<>();
         }
 
         // 손님들의 선호 리스트 채우기
         for (int i = 1; i <= N; i++) {
-            int dishes = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int dishes = Integer.parseInt(st.nextToken());
             for (int j = 0; j < dishes; j++) {
-                int now = sc.nextInt();
+                int now = Integer.parseInt(st.nextToken());
                 sushi[now].add(i);
             }
         }
 
         // 요리사가 초밥을 만드는 과정
+        st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= M; i++) {
-            int now = sc.nextInt(); // 요리사가 현재 만든 초밥
+            int now = Integer.parseInt(st.nextToken()); // 요리사가 현재 만든 초밥
             if (sushi[now].size() > 0) { // 초밥을 먹을 사람이 있으면
                 int eatPerson = sushi[now].remove(0);
                 eat[eatPerson]++;
             }
         }
 
-        // 손님별 먹은 초밥 수 출력
         for (int i = 1; i <= N; i++) {
-            System.out.print(eat[i] + " ");
+            sb.append(eat[i]).append(" ");
         }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
