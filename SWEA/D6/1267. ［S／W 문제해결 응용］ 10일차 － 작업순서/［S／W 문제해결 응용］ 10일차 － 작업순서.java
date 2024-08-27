5fc3753e48761d9b49e8c2094d43e83f86d1c289
이정빈 <alenjb@naver.com>
2024-08-27 19:51:07 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 163 ms, Memory: 24,852 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -8,65 +8,66 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution {
-    static Queue<Integer> q = new ArrayDeque<>();
-    static ArrayList<Integer> [] next;
-    static int [] inDegree;
+    static ArrayList<Integer>[] next;
+    static int[] inDegree;
     static int v;
-    static boolean [] visited;
     static StringBuilder sb;
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         sb = new StringBuilder();
 
-
-        for(int tc=1; tc<=10; tc++) {
+        for (int tc = 1; tc <= 10; tc++) {
             sb.append("#").append(tc).append(" ");
             // 입력받기
             st = new StringTokenizer(br.readLine());
             v = Integer.parseInt(st.nextToken()); // 정점의 개수
             int e = Integer.parseInt(st.nextToken()); // 간선의 개수
 
-            next = new ArrayList [v+1];
-            visited = new boolean [v+1];
-            inDegree = new int [v+1];
-            for(int i=0; i<=v; i++) {
+            next = new ArrayList[v + 1];
+            inDegree = new int[v + 1];
+            for (int i = 0; i <= v; i++) {
                 next[i] = new ArrayList<>();
             }
 
             st = new StringTokenizer(br.readLine());
-            for(int i=0; i<e; i++) {
+            for (int i = 0; i < e; i++) {
                 int from = Integer.parseInt(st.nextToken());
                 int end = Integer.parseInt(st.nextToken());
                 next[from].add(end);
                 inDegree[end]++;
             }
+
             topSort();
         }
         bw.write(sb.toString());
         bw.flush();
         bw.close();
-
     }
 
     private static void topSort() {
-
-        for(int i=1; i<=v; i++) {
-            if(inDegree[i] == 0 && !visited[i]) {
+        Queue<Integer> q = new ArrayDeque<>();
+        
+        // 진입 차수가 0인 노드를 큐에 삽입
+        for (int i = 1; i <= v; i++) {
+            if (inDegree[i] == 0) {
                 q.add(i);
-                while(!q.isEmpty()) {
-                    int now = q.poll();
-                    visited[now] = true;
-                    sb.append(now+" ");
-                    for(int num : next[now]) {
-                        inDegree[num] --;
-                        //진입 차수 0인거 찾기
-                        if(inDegree[num] == 0 && !visited[num]) {
-                            q.add(num);
-                        }
-                    }
+            }
+        }
+
+        // 큐가 빌 때까지 반복
+        while (!q.isEmpty()) {
+            int now = q.poll();
+            sb.append(now).append(" ");
+            
+            for (int num : next[now]) {
+                inDegree[num]--;
+                
+                // 진입 차수가 0이 되면 큐에 삽입
+                if (inDegree[num] == 0) {
+                    q.add(num);
                 }
             }
         }
