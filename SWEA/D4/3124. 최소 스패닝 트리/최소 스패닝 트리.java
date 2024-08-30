d1f97ae29fe0850e8cd525509f094fffe5f92d9b
이정빈 <alenjb@naver.com>
2024-08-30 14:43:02 +0900

    [D4] Title: 최소 스패닝 트리, Time: 4,631 ms, Memory: 244,576 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,84 +1,79 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
-    static int [] parent;
-    static int [][] arr;
-    static int V, E;
-    static PriorityQueue<Node7> pq = new PriorityQueue<>();
-    public static void main(String[] args) throws Exception {
+    static List<Node10> [] arr;
+    static boolean [] visited;
+    static PriorityQueue<Node10> pq = new PriorityQueue<>();
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
         st = new StringTokenizer(br.readLine());
         int T = Integer.parseInt(st.nextToken());
 
-        for(int tc=1; tc<=T; tc++){
+        for(int tc = 1; tc<=T; tc++){
             st = new StringTokenizer(br.readLine());
-            V = Integer.parseInt(st.nextToken());
-            E = Integer.parseInt(st.nextToken());
-            parent = new int[V+1];
-
-            for(int i=1; i<=V; i++) parent[i] = i;
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+            arr = new List[V+1];
+            for(int i=1; i<=V; i++) arr[i] = new ArrayList<>();
+            visited = new boolean[V+1];
 
             for(int i=0; i<E; i++){
                 st = new StringTokenizer(br.readLine());
                 int s = Integer.parseInt(st.nextToken());
                 int e = Integer.parseInt(st.nextToken());
                 int w = Integer.parseInt(st.nextToken());
-                pq.add(new Node7(s,e,w));
+                Node10 node1 = new Node10(s, e, w);
+                Node10 node2 = new Node10(e, s, w);
+                arr[s].add(node1);
+                arr[e].add(node2);
             }
+
+            for(Node10 n : arr[1]){
+                pq.add(n);
+            }
+            visited[1] = true;
 
             int cnt = 0;
             long result = 0;
-            while (cnt<V-1){
-                Node7 poll = pq.poll();
-                int rootS = find(poll.start);
-                int rootE = find(poll.end);
+            while (cnt < V-1){
+                Node10 poll = pq.poll();
+                if(visited[poll.e])continue;
+                cnt++;
+                visited[poll.e] = true;
+                result+=poll.w;
 
-                if(rootS != rootE) {
-                    union(poll.start, poll.end);
-                    result += poll.weight;
-                    cnt++;
+                for(Node10 n : arr[poll.e]){
+                    pq.add(n);
                 }
             }
+            pq.clear();
             sb.append("#").append(tc).append(" ").append(result).append("\n");
         }
+
         System.out.print(sb);
-
-    }
-    static void union (int a, int b){
-        int rootA = find(a);
-        int rootB = find(b);
-
-        if(rootA ==  rootB) return;
-        parent[rootB] = rootA;
-    }
-    static int find(int num){
-        if(num == parent[num]) return num;
-        return parent[num] = find(parent[num]);
     }
 }
-
-class Node7 implements Comparable<Node7>{
-    int start;
-    int end;
-    int weight;
-
-    public Node7(int start, int end, int weight) {
-        this.start = start;
-        this.end = end;
-        this.weight = weight;
-    }
+class Node10 implements Comparable<Node10>{
+    int s;
+    int e;
+    int w;
 
     @Override
-    public int compareTo(Node7 o) {
-        return this.weight - o.weight;
+    public int compareTo(Node10 o){
+        return Integer.compare(w, o.w);
+    }
+
+    public Node10(int s, int e, int w) {
+        this.s = s;
+        this.e = e;
+        this.w = w;
     }
 }
