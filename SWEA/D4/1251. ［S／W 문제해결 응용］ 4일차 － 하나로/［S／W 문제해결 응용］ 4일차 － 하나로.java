f591bf410cec1d64d3ca7da117030c5962953986
이정빈 <alenjb@naver.com>
2024-08-30 17:37:13 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 641 ms, Memory: 114,692 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,107 +1,88 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
+    static int [] xPos, yPos;
+    static List<Node> [] arr;
     static int N;
-    static PriorityQueue<Node8> dist;
+    static boolean [] visited;
     static double E;
-    static int[] parent;
-    static int[] xs, ys;
-
-    public static void main(String[] args) throws Exception {
+    static PriorityQueue<Node> pq = new PriorityQueue<>();
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st ;
 
         st = new StringTokenizer(br.readLine());
         int T = Integer.parseInt(st.nextToken());
 
-        for (int tc = 1; tc <= T; tc++) {
+        for(int tc=1; tc<=T; tc++){
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
-            parent = new int[N + 1];
-            xs = new int[N + 1];
-            ys = new int[N + 1];
-            dist = new PriorityQueue<>();
+            xPos = new int[N];
+            yPos = new int[N];
+            arr = new List[N+1];
+            visited = new boolean[N+1];
 
-            for (int i = 1; i <= N; i++) parent[i] = i;
+            for(int i=0; i<=N; i++) arr[i] = new ArrayList<>();
 
-            // 노드 위치 저장
             st = new StringTokenizer(br.readLine());
-            for (int i = 1; i <= N; i++) {
-                xs[i] = Integer.parseInt(st.nextToken());
-            }
-            st = new StringTokenizer(br.readLine());
-            for (int i = 1; i <= N; i++) {
-                ys[i] = Integer.parseInt(st.nextToken());
-            }
+            for(int i=0; i<N; i++) xPos[i] = Integer.parseInt(st.nextToken());
 
-            // 가중치 입력
+            st = new StringTokenizer(br.readLine());
+            for(int i=0; i<N; i++) yPos[i] = Integer.parseInt(st.nextToken());
+
             st = new StringTokenizer(br.readLine());
             E = Double.parseDouble(st.nextToken());
 
-            // 거리 계산
-            for (int i = 1; i <= N; i++) {
-                for (int j = i + 1; j <= N; j++) {
-                    int x1 = xs[i];
-                    int y1 = ys[i];
-                    int x2 = xs[j];
-                    int y2 = ys[j];
-                    double w = (Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)) * E;
-                    dist.add(new Node8(i, j, w));
+            for (int i=0; i<N; i++){ // i: start 노드 (yPos[i] xPos[i])
+                for(int j= i+1; j<N; j++){ // j: end 노드의 (yPos[j] xPos[j])
+                    int x1 = xPos[i];
+                    int y1 = yPos[i];
+                    int x2 = xPos[j];
+                    int y2 = yPos[j];
+
+                    double cost = (Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2)) * E;
+                    arr[i].add(new Node(i, j, cost));
+                    arr[j].add(new Node(j, i, cost));
                 }
             }
-
+            pq.add(new Node(0, 0, 0));
             int cnt = 0;
             double result = 0;
-            while (cnt < N - 1) {
-                Node8 poll = dist.poll();
-                int start = poll.s;
-                int end = poll.e;
 
-                int rootS = find(start);
-                int rootE = find(end);
-
-                if (rootS != rootE) {
-                    union(start, end);
-                    result += poll.w;
-                    cnt++;
-                }
+            while (cnt <= N-1){
+                Node poll = pq.poll();
+                if(visited[poll.e]) continue;
+                visited[poll.e] = true;
+                cnt++;
+                result += poll.w;
+                for(Node next : arr[poll.e]) pq.add(next);
             }
-
+            pq.clear();
             sb.append("#").append(tc).append(" ").append(Math.round(result)).append("\n");
         }
-
         System.out.print(sb);
     }
 
-    static void union(int a, int b) {
-        int rootA = find(a);
-        int rootB = find(b);
+    static class Node implements Comparable<Node>{
+        int s;
+        int e;
+        double w;
 
-        if (rootA != rootB) parent[rootB] = rootA;
-    }
+        public Node(int s, int e, double w) {
+            this.s = s;
+            this.e = e;
+            this.w = w;
+        }
 
-    static int find(int num) {
-        if (num == parent[num]) return num;
-        return parent[num] = find(parent[num]);
-    }
-}
-
-class Node8 implements Comparable<Node8> {
-    int s, e;
-    double w;
-
-    public Node8(int s, int e, double w) {
-        this.s = s;
-        this.e = e;
-        this.w = w;
-    }
-
-    @Override
-    public int compareTo(Node8 o) {
-        return Double.compare(this.w, o.w);
+        @Override
+        public int compareTo(Node o) {
+            return Double.compare(this.w, o.w);
+        }
     }
 }
