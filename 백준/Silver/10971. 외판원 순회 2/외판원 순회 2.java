cda3deee0c8f8ab6e188ac09faa53138c2106e3c
이정빈 <alenjb@naver.com>
2024-09-02 15:17:41 +0900

    [Silver II] Title: 외판원 순회 2, Time: 100 ms, Memory: 12208 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,13 +1,12 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N;
     static long[][] arr;
-    static List<int[]> perms = new ArrayList<>();
+    static boolean[] visited;
+    static long result = Long.MAX_VALUE;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,6 +14,7 @@ public class Main {
 
         N = Integer.parseInt(br.readLine());
         arr = new long[N][N];
+        visited = new boolean[N];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -23,48 +23,26 @@ public class Main {
             }
         }
 
-        // 순열 만들기
-        List<Integer> poss = new ArrayList<>();
-        for (int i = 0; i < N; i++) poss.add(i);
-        permutation(0, new int[N], poss);
-
-        long result = Long.MAX_VALUE;
-
-        // 순열에서 빼서 계산하기
-        for (int[] nows : perms) {
-            long tmpResult = 0;
-            boolean valid = true;
-
-            for (int k = 0; k < N - 1; k++) {
-                int start = nows[k];
-                int end = nows[k + 1];
-                if (arr[start][end] == 0) {
-                    valid = false; // 이동할 수 없는 경로가 존재하는 경우
-                    break;
-                }
-                tmpResult += arr[start][end];
-            }
-
-            if (valid && arr[nows[N - 1]][nows[0]] != 0) { // 마지막에서 첫 번째 도시로 돌아가는 경로가 있는지 확인
-                tmpResult += arr[nows[N - 1]][nows[0]];
-                result = Math.min(result, tmpResult);
-            }
-        }
+        visited[0] = true;  // 출발점을 0번 도시로 고정
+        dfs(0, 0, 0, 1);  // 도시 0에서 시작
 
         System.out.println(result);
     }
 
-    private static void permutation(int cnt, int[] now, List<Integer> remain) {
-        if (cnt == N) {
-            perms.add(now.clone());
+    private static void dfs(int start, int now, long cost, int count) {
+        if (count == N) {
+            if (arr[now][start] != 0) {  // 마지막 도시에서 시작 도시로 돌아갈 수 있는 경우
+                result = Math.min(result, cost + arr[now][start]);
+            }
             return;
         }
-        for (int i = 0; i < remain.size(); i++) {
-            int num = remain.get(i);
-            now[cnt] = num;
-            remain.remove(i);
-            permutation(cnt + 1, now, remain);
-            remain.add(i, num);  // 원상태로 복구
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i] && arr[now][i] != 0) {
+                visited[i] = true;
+                dfs(start, i, cost + arr[now][i], count + 1);
+                visited[i] = false;
+            }
         }
     }
 }
