e594764d7463ee2ae2a4eedd1971c0d9e2aa6882
이정빈 <alenjb@naver.com>
2024-09-02 19:24:22 +0900

    [Gold IV] Title: 최단경로, Time: 972 ms, Memory: 128440 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,79 +1,78 @@
+import javax.crypto.Cipher;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        
-        st = new StringTokenizer(br.readLine());
-        int V = Integer.parseInt(st.nextToken()); // 노드의 개수
-        int E = Integer.parseInt(st.nextToken()); // 엣지의 개수
-        
-        st = new StringTokenizer(br.readLine());
-        int start = Integer.parseInt(st.nextToken()); // 출발 노드
-        
-        // 인접 리스트 만들기
-        List<Node>[] arr = new List[V + 1];
-        for (int i = 1; i <= V; i++) {
-            arr[i] = new ArrayList<>();
+    static class Edge implements Comparable<Edge> {
+        int node, cost;
+
+        public Edge(int node, int cost) {
+            this.node = node;
+            this.cost = cost;
         }
-        
-        // 엣지 입력 받기
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken()); // 시작 노드
-            int v = Integer.parseInt(st.nextToken()); // 도착 노드
-            int w = Integer.parseInt(st.nextToken()); // 가중치
-            arr[u].add(new Node(v, w));
+
+        @Override
+        public int compareTo(Edge o) {
+            return Integer.compare(cost, o.cost);
         }
-        
-        // 최단거리 배열
-        long [] D = new long [V+1];
-        boolean [] visited = new boolean[V+1];
-        for(int i=0; i<V+1; i++) D[i] = Long.MAX_VALUE;
-        D[start] = 0;
-        
-        PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> (int)(o1.weight - o2.weight));
-        
-        pq.add(new Node(start, 0));
-        
-        while(!pq.isEmpty()) {
-        	Node node = pq.poll();
-        	if(visited[node.vertex]) continue; //이미 방문했었으면 안가기
-        	visited[node.vertex] = true;
-        	for(Node n : arr[node.vertex]) {
-        		if(D[n.vertex] > (D[node.vertex] + n.weight)) {
-        			D[n.vertex] = D[node.vertex] + n.weight;
-        			pq.add(new Node(n.vertex, D[n.vertex]));
-        		}
-        	}
-        }
-        
-        for(int i=1; i<V+1; i++) {
-        	if(D[i] == Long.MAX_VALUE) sb.append("INF\n");
-        	else sb.append(D[i]+"\n");
-        }
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        
-	}
-}
-class Node {
-    int vertex;
-    long weight;
-    
-    public Node(int vertex, long weight) {
-        this.vertex = vertex;
-        this.weight = weight;
     }
- }
+
+    static int V, E, K;
+    static int[] dist;
+    static ArrayList<ArrayList<Edge>> adj;
+    static PriorityQueue<Edge> pq = new PriorityQueue<>();
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] VE = br.readLine().split(" ");
+        V = Integer.parseInt(VE[0]);
+        E = Integer.parseInt(VE[1]);
+        K = Integer.parseInt(br.readLine());
+        dist = new int[V + 1];
+        visited = new boolean[V + 1];
+        adj = new ArrayList<>();
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        for (int i = 0; i <= V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < E; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken()); // start -> end
+            int cost = Integer.parseInt(st.nextToken());
+            adj.get(start).add(new Edge(end, cost)); // b로가는 가중치
+        }
+
+        pq.add(new Edge(K, 0));
+        dist[K] = 0;
+
+        while (!pq.isEmpty()) {
+            Edge current = pq.poll();
+            if(visited[current.node]) continue;
+            visited[current.node] = true;
+            for (Edge next : adj.get(current.node)) {
+                if (dist[next.node] > dist[current.node] + next.cost) {
+                    dist[next.node] = dist[current.node] + next.cost;
+                    pq.add(new Edge(next.node, dist[next.node])); // 새로운 노드를 업데이트
+                }
+            }
+        }
+
+
+        for (int i = 1; i <= V; i++) {
+            if (dist[i] == Integer.MAX_VALUE) {
+                System.out.println("INF");
+            } else {
+                System.out.println(dist[i]);
+            }
+        }
+    }
+}
