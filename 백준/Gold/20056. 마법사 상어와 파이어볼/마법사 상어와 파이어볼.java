c5316768c11876ae07b09a7f8e5f334fe1b4fa7d
이정빈 <alenjb@naver.com>
2024-09-07 16:16:05 +0900

    [Gold IV] Title: 마법사 상어와 파이어볼, Time: 760 ms, Memory: 171868 KB -BaekjoonHub

---
+++ 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.java
@@ -0,0 +1,134 @@
+import java.util.*;
+
+public class Main {
+    static int N, M, K;
+    static List<Fireball>[][] arr;
+    static Set<List<Integer>> visited;
+    static Queue<Fireball> q;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        K = sc.nextInt();
+
+        q = new ArrayDeque<>();
+        for(int i=0; i<M; i++){
+            int r = sc.nextInt();
+            int c = sc.nextInt();
+            int mass = sc.nextInt();
+            int speed = sc.nextInt();
+            int direction = sc.nextInt();
+            q.add(new Fireball(r, c, mass, speed, direction));
+        }
+        for(int i=0; i<K; i++) {
+            arr = new List[N][N];
+            visited = new HashSet<>();
+            for(int a=0;a<N; a++){
+                for(int b=0; b<N; b++){
+                    arr[a][b] = new ArrayList<>();
+                }
+            }
+            int size = q.size();
+            for (int j = 0; j < size; j++) {
+                Fireball poll = q.poll();
+                move(poll); //움직이기
+            }
+            q.clear();
+            for(List<Integer> pos : visited){
+                int pR = pos.get(0);
+                int pC = pos.get(1);
+                if (arr[pR][pC].size() > 1) { //한칸에 여러개가 있으면
+                    update(pR, pC);
+                } else q.add(arr[pR][pC].get(0));
+            }
+        }
+
+        long result = 0;
+        for (Fireball fireball : q) {
+            result += fireball.mass;
+        }
+        System.out.println(result);
+
+    }
+
+    static void move(Fireball fireball){
+        int direction = fireball.direction;
+        int speed = fireball.speed;
+        int newR = fireball.r;
+        int newC = fireball.c;
+        // 열
+        if(0 < direction && direction < 4) newC+=speed;
+        else if(4 < direction && direction < 8) newC+=-speed;
+
+        //행
+        if(3 <= direction && direction <= 5) newR+=speed;
+        else if(direction == 0 || direction == 1 || direction == 7) newR+=-speed;
+
+        newR %= N;
+        newC %= N;
+        if(newR <0) newR += N;
+        if(newC <0) newC += N;
+        arr[newR][newC].add(new Fireball(newR, newC, fireball.mass, fireball.speed, fireball.direction));
+        visited.add(new ArrayList<>(Arrays.asList(newR, newC)));
+    }
+
+    // 두개 이상이 한칸인 경우
+    static void update(int r, int c){
+        int odd = 0;
+        int even = 0;
+
+        int massSum = 0;
+        int speedSum = 0;
+        int size = arr[r][c].size();
+        for (Fireball fireball : arr[r][c]) {
+            massSum += fireball.mass;;
+            speedSum += fireball.speed;
+            if(fireball.direction % 2 == 0) even++;
+            else odd++;
+        }
+        // 질량
+        massSum /=5;
+        if(massSum ==0) return;
+        // 속력
+        speedSum /= size;
+        List<Integer> d = new ArrayList<>();
+        //방향
+        if(odd == size || even == size){
+            d.add(0);
+            d.add(2);
+            d.add(4);
+            d.add(6);
+        }else {
+            d.add(1);
+            d.add(3);
+            d.add(5);
+            d.add(7);
+        }
+
+        // 분화
+        for(int i=0; i<4; i++){
+            q.add(new Fireball(r,c,massSum, speedSum, d.get(i)));
+        }
+    }
+
+    static class Fireball{
+        int r;
+        int c;
+        int mass; //질량
+        int speed; // 속력
+        int direction; //방향
+
+        public Fireball(int r, int c, int mass, int speed, int direction) {
+            this.r = r;
+            this.c = c;
+            this.mass = mass;
+            this.speed = speed;
+            this.direction = direction;
+        }
+
+        @Override
+        public String toString() {
+            return "r: "+r +" c: "+c+" mass : "+mass + " speed: "+speed + " direction: "+ direction;
+        }
+    }
+}
