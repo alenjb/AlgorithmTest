c476f56d14badb8723099de8eee362a6dbe5e626
이정빈 <alenjb@naver.com>
2024-09-14 23:14:39 +0900

    [Gold III] Title: 고기잡이, Time: 212 ms, Memory: 12396 KB -BaekjoonHub

--- 백준/Gold/7573. 고기잡이/고기잡이.java
+++ 백준/Gold/7573. 고기잡이/고기잡이.java
@@ -2,27 +2,26 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N,l,M;
+    static int l,M;
     static int result = 0;
-    static ArrayList<Pos> fishes = new ArrayList<>();
+    static ArrayList<Pos> fishes = new ArrayList<>(); // 물고기 위치 저장
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
         l = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        // 1,1 부터 시작
         for(int i=0;i<M;i++){
             st = new StringTokenizer(br.readLine());
-            int y = Integer.parseInt(st.nextToken());
-            int x = Integer.parseInt(st.nextToken());
-            fishes.add(new Pos(y,x));
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            fishes.add(new Pos(r,c));
         }
 
-        for(int i=0;i<M;i++){
-            for(int j=0;j<M;j++){
+        for(int i=0; i<M; i++){
+            for(int j=0; j<M; j++){
                 catchFish(i,j);
             }
         }
@@ -31,6 +30,7 @@ public class Main {
     }
 
     static void catchFish(int fish1, int fish2){
+        // 최대한 많은 물고기를 잡기 위해서는 그물의 모서리나 변이 물고기들과 맞닿아 있는 것이 유리하므로
         int r = fishes.get(fish1).r; // y 좌표 기준 물고기
         int c = fishes.get(fish2).c; // x좌표 기준 물고기
 
@@ -38,25 +38,20 @@ public class Main {
         int height = ( l - 2 * width ) / 2;
 
         while(height>0){
-            // 잡을 수 있는 물고기 수
             int cnt = 0;
 
             for(int i=0;i<M;i++){
                 if(
-                    // 두 물고기 중 한 물고기를 기준으로 그물 길이만큼의 x 길이 내에 물고기가 존재하는지 확인
-                        c <= fishes.get(i).c &&
-                                fishes.get(i).c <= c + height &&
-                                // 두 물고기 중 한 물고기를 기준으로 그물 길이만큼의 y 길이 내에 물고기가 존재하는지 확인
-                                r <= fishes.get(i).r &&
-                                fishes.get(i).r <= r + width
+                    // x좌표 검사
+                        c <= fishes.get(i).c && fishes.get(i).c <= c + height &&
+                                // y좌표 검사
+                                r <= fishes.get(i).r && fishes.get(i).r <= r + width
                 )
                     cnt++;
             }
 
-            // 현재 물고기 수와 답과의 최대값 구하기
-            if(result < cnt) result = cnt;
+            result = Math.max(result, cnt);
 
-            // 다른 사이즈의 그물로 변환
             width++;
             height = (l - 2*width)/2;
         }
