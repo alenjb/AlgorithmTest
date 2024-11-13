c518c515e0f24cdc626789996ad58502e7681693
이정빈 <alenjb@naver.com>
2024-11-13 16:47:05 +0900

    [Gold III] Title: 줄 세우기, Time: 840 ms, Memory: 159936 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,58 +1,45 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
+	static int N, M;
+	static int[] indegree;
+	static List<Integer>[] arr;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
-	
+		N = sc.nextInt();
+		M = sc.nextInt();
+		indegree= new int [N+1];
+		arr = new List[N+1];
+		for(int i=1; i<=N; i++) arr[i] = new ArrayList<Integer>();
 		
 		
-		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());	// 학생의 수
-		int M = Integer.parseInt(st.nextToken());	//키를 비교한 횟수
-		
-		List<Integer> [] arr = new ArrayList[N+1]; // 인접 리스트
-		int [] in = new int[N+1];	// 진입 차수 배열
-		
-		//인접리스트 초기화
-		for(int i=0; i<=N; i++) {
-			arr[i] = new ArrayList<>();
+		for(int i=0; i<M; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			arr[a].add(b);
+			indegree[b]++;
 		}
 		
-		for(int tc=0; tc<M; tc++) {
-			st = new StringTokenizer(br.readLine());
-			int first = Integer.parseInt(st.nextToken());	
-			int second = Integer.parseInt(st.nextToken());
-			arr[first].add(second); // 인접리스트에 추가
-			in[second]++; //진입 차수 증가
-		}
-		
-		Queue<Integer> q = new LinkedList<>();
-		for(int i=1; i<N+1; i++) {
-			if(in[i] == 0) q.add(i);
+		Queue<Integer> q = new ArrayDeque<Integer>();
+		for(int i=1; i<=N; i++) {
+			if(indegree[i] == 0) q.add(i);
 		}
 		
 		while(!q.isEmpty()) {
 			int now = q.poll();
-			sb.append(now+" ");
-			for(int num : arr[now]) {
-				in[num]--;
-				if(in[num] == 0) q.add(num);
+			sb.append(now).append(" ");
+			for(int n: arr[now]) {
+				indegree[n] --;
+				if(indegree[n] == 0) q.add(n);
 			}
 		}
 		
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
+		System.out.println(sb);
 	}
+
 }
