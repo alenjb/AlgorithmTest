7a19c3ad847d141d6364ffa3ab3727c13ab80463
이정빈 <alenjb@naver.com>
2025-01-29 16:38:25 +0900

    [level 2] Title: 올바른 괄호, Time: 24.59 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,24 +1,18 @@
 import java.util.*;
 class Solution {
     boolean solution(String s) {
+        Stack<Character> st = new Stack<>();
         boolean answer = true;
-
-        // Stack<Charater> lp = new Stack<>();
-        // Stack<Charater> rp = new Stack<>();
-        Stack<Character> stack = new Stack<>();
         for(int i=0; i<s.length(); i++){
-            if(s.charAt(i) == '('){
-                stack.push('(');
-            }
-            else{ // ')'인 경우
-                if(stack.isEmpty()){
-                    return false;
-                }else if(stack.peek() == '('){
-                    stack.pop();
-                }
+            char c = s.charAt(i);
+            if(st.isEmpty() || c == '(') st.push(c);
+            else if(c == ')' && st.peek() == '('){
+                st.pop();
+            }else {
+                answer = false;
+                break;
             }
         }
-        if(!stack.isEmpty()) answer = false;
-        return answer;
+        return answer && st.isEmpty();
     }
 }
