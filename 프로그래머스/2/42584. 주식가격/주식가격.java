b1049e6f8beabb2a3940eb8bfe9c4953a29f3bd3
이정빈 <alenjb@naver.com>
2025-02-05 22:12:32 +0900

    [level 2] Title: 주식가격, Time: 51.87 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,41 +1,28 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] prices) {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        int len = prices.length;
-        int[] answer = new int [len];
-        int idx = 0;
+        int n = prices.length;
+        int [] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
         
-        pq.add(new Node(0, prices[0]));
-        for(int i=1; i< prices.length; i++) {
-            pq.add(new Node(i, prices[i]));
-            int dist = prices[i] - prices[i-1];//i-1에서 i까지의 증감 
-            while(dist <0 &&!pq.isEmpty()&& pq.peek().price > prices[i] 
-                  && pq.peek().num < i){// 작아졌으면
-                Node n = pq.poll();
-                answer[n.num] = i - n.num;
-            }    
+        for(int i=1; i<n; i++){
+            int now = prices[i];
+            if(!stack.isEmpty() && prices[stack.peek()] > now){
+                while(!stack.isEmpty() && prices[stack.peek()] > now){
+                    // 빼기
+                    answer[stack.peek()] = i - stack.peek();
+                    stack.pop();
+                }
+            }
+            stack.push(i);
         }
-        while(!pq.isEmpty()){
-            Node n = pq.poll();
-            answer[n.num] = len-1 - n.num;
+        if(!stack.isEmpty()){
+            while(!stack.isEmpty()){
+                int now = stack.pop();
+                answer[now] = n-1 - now;                
+            }
         }
-        
         return answer;
-    }
-}
-
-class Node implements Comparable<Node>{
-    int num;
-    int price;
-    
-    public Node(int n, int p){
-        this.num = n;
-        this.price = p;
-    }
-    
-    public int compareTo(Node n){
-        return n.price - this.price;
     }
 }
