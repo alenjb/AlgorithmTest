8aa9cde3e300d3bf49f62de3b8dfe597230e459c
이정빈 <alenjb@naver.com>
2025-04-10 23:10:53 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.23 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,34 +1,49 @@
 import java.util.*;
 class Solution {
-    public int solution(int[][] maps) {
-        int answer = -1;
-        int r = maps.length;
-        int c = maps[0].length;
-        
-        int [] dr = {0, 1, -1, 0};
-        int [] dc = {-1, 0, 0, 1};
-        
-        Queue<int []> q = new ArrayDeque<>();
-        boolean [][] visited = new boolean[r][c];
-        q.add(new int[]{0, 0, 0}); //r, c, cnt
+    static int answer = Integer.MAX_VALUE;
+    static int r = 0;
+    static int c = 0;
+    static int [] dr = {0, -1, 1, 0}; // 왼 위 아래 오른
+    static int [] dc = {-1, 0, 0, 1}; // 왼 위 아래 오른
+	public int solution(int[][] maps) {
+        r = maps.length;
+        c = maps[0].length;        
+        BFS(maps);
+
+        if(answer == Integer.MAX_VALUE) return -1;
+		return answer;
+	}
+    
+    //BFS
+    public void BFS(int[][]maps){
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{0,0,1});
+        boolean [][] visited = new boolean [r][c];
+        visited[0][0] = true;
+
         while(!q.isEmpty()){
             int [] now = q.poll();
-            if(visited[now[0]][now[1]]) continue;
-            visited[now[0]][now[1]] = true;
+            int nowR = now[0];
+            int nowC = now[1];
+            int cost = now[2];
             
-            now[2]++;
-            if(now[0] == r-1 && now[1] == c-1){
-                answer = now[2];
-                break;
+            // r-1, c-1에 도달하면 종료
+            if(nowR == r-1&& nowC == c-1){
+                answer = Math.min(answer, cost);
+                return;
             }
             
+            
+            // 동서남북으로 BFS
             for(int i=0; i<4; i++){
-                int newR = now[0] + dr[i];
-                int newC = now[1] + dc[i];
-                if(newR >=0 && newR < r && newC >=0 && newC < c && maps[newR][newC]!=0 && !visited[newR][newC]) q.add(new int []{newR, newC, now[2]});
+                int newR = nowR + dr[i];
+                int newC = nowC + dc[i];
+                if(newR >= 0 && r > newR && newC >=0 && newC <c && !visited[newR][newC]
+                  && maps[newR][newC] == 1){
+                    visited[newR][newC] = true;
+                    q.add(new int[]{newR, newC, cost+1});
+                }
             }
-            
         }
-        return answer;
     }
 }
