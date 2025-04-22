e47273293ccb75383d8eb03284284edf60c9ae7b
이정빈 <alenjb@naver.com>
2025-04-22 23:15:40 +0900

    [level 3] Title: 입국심사, Time: 68.83 ms, Memory: 99.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,28 +1,22 @@
 import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
-        long max = Integer.MIN_VALUE;
-        for(int i=0; i<times.length; i++){
-            max = Math.max(times[i], max);
-        }
-        return search(n, times, max * n);
-    }
-    
-    public long search(int n, int[] times, long max){
-        long right = max;// 정답 범위 포함
         long left = 0;
-        while(left +1 < right){
-            long mid = (right + left) / 2;
+        long right = (long) Arrays.stream(times).max().getAsInt() * n; // 수정된 부분
+        
+        while(left + 1 < right){
+            long mid = (left + right) / 2;
             if(isValid(mid, times, n)) right = mid;
             else left = mid;
         }
         return right;
     }
-    public boolean isValid(long num, int[] times, int n){
-        long valid = 0; // 처리 가능 사람 수
-        for(int time : times){
-            valid += (num / time);
+
+    public boolean isValid(long time, int[] times, int n){
+        long avail = 0;
+        for(int t : times){
+            avail += time / t;
         }
-        return valid >= n;
+        return avail >= n;
     }
 }
