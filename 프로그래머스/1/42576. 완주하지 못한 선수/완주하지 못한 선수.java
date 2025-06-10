527d85edc33bcd6ee27b2bef65062133df61161a
이정빈 <alenjb@naver.com>
2025-06-10 19:50:11 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.23 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,22 +1,21 @@
 import java.util.*;
-
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        Map<String, Integer> h = new HashMap<>();
-        for (String name : participant) {
-            if (h.get(name) != null) {
-                h.put(name, h.get(name) + 1);
-            } else {
-                h.put(name, 1);
+        Map<String, Integer> comp = new HashMap<>(); // 완주한 사람
+        
+        for(String s : completion) {
+            if(comp.containsKey(s)) comp.put(s, comp.get(s) + 1);
+            else comp.put(s, 1);
+        } 
+        
+        for(String p : participant){
+            if(!comp.containsKey(p)) return p;
+            else{
+                int num = comp.get(p);
+                if(num <=0) return p;
+                comp.put(p, num - 1);   
             }
         }
-        for (String name1 : completion) {
-            h.put(name1, h.get(name1) - 1);
-        }
-        for(String n : h.keySet()){
-            if (h.get(n) == 1) answer = n;
-        }
-        return answer;
+        return null;
     }
 }
