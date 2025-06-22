cde5b5bf200f9f07369519404fa7d02776e2da18
이정빈 <alenjb@naver.com>
2025-06-22 17:47:57 +0900

    [level 3] Title: 이중우선순위큐, Time: 66.98 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,40 +1,27 @@
 import java.util.*;
-import java.io.*;
-
 class Solution {
     public int[] solution(String[] operations) {
-        List<Integer> list = new ArrayList<>();
-        StringTokenizer st;
+        PriorityQueue<Integer> small = new PriorityQueue<Integer>();
+        PriorityQueue<Integer> big = new PriorityQueue<Integer>(Comparator.reverseOrder());
         
-        for(int i=0; i<operations.length; i++){
-            st = new StringTokenizer(operations[i]);
-            String op = st.nextToken();
-            int num = Integer.parseInt(st.nextToken());
-            if(op.equals("I")) {
-                list.add(num);
-                Collections.sort(list);
-            }
-            else if(op.equals("D")){
-                if(num == -1 && list.size()>0){
-                    list.remove(0);
-                }else if(list.size()>0){
-                    list.remove(list.size()-1);
+        for(String s : operations){
+            int l = s.length();
+            if(s.charAt(0) == 'I'){
+                int num = Integer.parseInt(s.substring(2, s.length()));
+                big.add(num);
+                small.add(num);
+            }else {                
+                if (s.equals("D 1")) { //최대 삭제
+                    Integer del = big.poll();
+                    small.remove(del);
+                }else if (s.equals("D -1")) { //최소 삭제
+                    Integer del = small.poll();
+                    big.remove(del);
                 }
             }
         }
-        int max = 0;
-        int min = 0;
-        if(list.size()>0){
-            if(list.size()>=2){
-                max = list.get(list.size()-1);
-                min = list.get(0);
-            }else{
-                int tmp = list.get(0);
-                max = tmp;
-                min = tmp;
-            }
-        }
-        int[] answer = {max, min};
+        if(small.isEmpty() && big.isEmpty()) return new int []{0,0};
+        int[] answer = new int []{big.poll(), small.poll()};
         return answer;
     }
 }
