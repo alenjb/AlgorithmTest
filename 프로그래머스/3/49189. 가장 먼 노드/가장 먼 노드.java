36bc5edf11234e283836bf0211db70b0056d1f0c
이정빈 <alenjb@naver.com>
2025-06-26 23:38:44 +0900

    [level 3] Title: 가장 먼 노드, Time: 32.00 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,12 +1,14 @@
 import java.util.*;
 class Solution {
-    static List<Integer>[] arr;
-    static int [] cost;
+    //BFS
     public int solution(int n, int[][] edge) {
-        arr = new List [n+1];
-        cost = new int [n+1];
-        for(int i=0; i<n+1; i++) arr[i] = new ArrayList<>();
-        int r = edge.length;        
+
+        int r = edge.length;
+        int c = edge[0].length;
+    
+        List<Integer> [] arr = new List[n+1];
+        boolean [] visited = new boolean [n+1];
+        for(int i=1; i<=n; i++) arr[i] = new ArrayList<>();
         for(int i=0; i<r; i++){
             int a = edge[i][0];
             int b = edge[i][1];
@@ -14,40 +16,41 @@ class Solution {
             arr[b].add(a);
         }
         
-        int answer = 0;
-        int max_val = -1;
-        Queue<int []> q = new ArrayDeque<>();
-        boolean [] visited = new boolean [n+1];
+        int max = 0;
+        Set<Integer> set = new HashSet<>();
         
-        q.add(new int []{1, 0});
+        //bfs
+        Queue<Node> q = new ArrayDeque<>();
+        q.add(new Node(1, 0));
         visited[1] = true;
-        //BFS
+        
         while(!q.isEmpty()){
-            int [] now = q.poll();
-            int pos = now[0];
-            int val = now[1];
-            
-            max_val = Math.max(val, max_val);
-            cost[pos] = val;
-            
-            for(int next : arr[pos]){
-                if (!visited[next]) {
+            Node poll = q.poll();
+            if(poll.cnt > max){
+                set = new HashSet<>();
+                set.add(poll.num);
+                max = poll.cnt;
+            }else if(poll.cnt == max){
+                set.add(poll.num);   
+            }
+                
+            for(int next : arr[poll.num]){
+                if(!visited[next]) {
+                    q.add(new Node(next, poll.cnt+1));
                     visited[next] = true;
-                    q.add(new int [] {next, val+1});                    
                 }
             }
         }
+        return set.size();
+    }
+    
+    static class Node{
+        int num;
+        int cnt;
         
-        // for(int nn : cost){
-        //     // System.out.println(nn);
-        //     if(max_val == nn) answer++;
-        // }
-        for(int i = 0; i < cost.length; i++) {
-            if(max_val == cost[i]) answer++;
+        public Node(int n, int c){
+            num = n;
+            cnt = c;
         }
-
-
-        
-        return answer;
     }
 }
