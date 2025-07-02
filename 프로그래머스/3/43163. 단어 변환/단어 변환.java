875459127d0375d30be94eef1740253919ba369f
이정빈 <alenjb@naver.com>
2025-07-02 22:26:24 +0900

    [level 3] Title: 단어 변환, Time: 1.26 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,61 +1,73 @@
 import java.util.*;
+
 class Solution {
-    static List<Integer> [] arr;
-    static int answer = Integer.MAX_VALUE;
     public int solution(String begin, String target, String[] words) {
         int l = words.length;
-        arr = new ArrayList[l+1];
-        for(int i=0; i<=l; i++) arr[i] = new ArrayList<Integer>();
-        // 1단어 차이인것 연결리스트로 만들기
-        for(int i=0; i<l; i++){
-            for(int j=0; j<l; j++){
-                if(i!=j && isOneDiff(words[i], words[j])){ // 1개만 다르면
-                    arr[i].add(j);
-                }
-            }
-        }
-        // VISITED 배열 만들기
-        boolean [] visited = new boolean[l+1]; // 마지막 인덱스는 begin
-        // begin에 대해 1개 차이인 것 연결
-        for(int i=0; i<l; i++){
-            if(isOneDiff(begin, words[i])){ // 1개만 다르면
-                    arr[l].add(i);
-            }
-        }
-        
-        Queue<Integer> q = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
-        q.add(l);
-        q2.add(0);
-        while(!q.isEmpty()){
-            int poll = q.poll();
-            int poll2 = q2.poll();
-            String word;
-            if(poll == l) word = begin;
-            else{
-                word = words[poll];            
-            }
+        int wl = begin.length();
 
-            if(!visited[poll] && word.equals(target)) return poll2;
-            visited[poll] = true;
-            for(int num : arr[poll]){
-                if(!visited[num]) {
-                    q.add(num);
-                    q2.add(poll2+1);
+        String[] allWords = new String[l + 1];
+        allWords[0] = begin;
+        for (int i = 0; i < l; i++) allWords[i + 1] = words[i];
+        l++;
+
+        boolean[] visited = new boolean[l];
+        Map<String, Integer> map = new HashMap<>();
+        List<Integer>[] arr = new List[l];
+        for (int i = 0; i < l; i++) {
+            arr[i] = new ArrayList<>();
+            map.put(allWords[i], i);
+        }
+
+        for (int i = 0; i < l; i++) {
+            for (int j = i + 1; j < l; j++) {
+                if (getDiff(allWords[i], allWords[j]) == 1) {
+                    arr[i].add(j);
+                    arr[j].add(i);
                 }
             }
         }
-        // BFS를 하면서 타겟이면 종료, visited이면 종료, 연결리스트가 없으면 종료
-        if(answer == Integer.MAX_VALUE) return 0;
-        return answer;
-    }
-        
-    // 같은 글자 개수 세는 함수(1개만 다르면 true 리턴)
-    public boolean isOneDiff(String w1, String w2){
-        int diff = 0;
-        for(int i=0; i<w1.length(); i++){
-            if(w1.charAt(i) != w2.charAt(i)) diff++;
+
+        // target이 words에 없으면 변환 불가
+        if (!map.containsKey(target)) return 0;
+
+        // BFS
+        Queue<Node> q = new ArrayDeque<>();
+        q.offer(new Node(begin, 0, 0)); // begin은 항상 0번
+
+        while (!q.isEmpty()) {
+            Node poll = q.poll();
+            if (visited[poll.num]) continue;
+            visited[poll.num] = true;
+
+            if (poll.s.equals(target)) return poll.cnt;
+
+            for (int next : arr[poll.num]) {
+                if (!visited[next]) {
+                    q.offer(new Node(allWords[next], poll.cnt + 1, next));
+                }
+            }
         }
-        return diff == 1;
+
+        return 0;
+    }
+
+    static int getDiff(String a, String b) {
+        int cnt = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) cnt++;
+        }
+        return cnt;
+    }
+
+    static class Node {
+        String s;
+        int cnt;
+        int num;
+
+        public Node(String s, int cnt, int num) {
+            this.s = s;
+            this.cnt = cnt;
+            this.num = num;
+        }
     }
 }
