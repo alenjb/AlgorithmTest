d98e5c86eb8b8c169f5305e4717ea1c0c23f69c1
이정빈 <alenjb@naver.com>
2025-07-05 15:56:10 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.04 ms, Memory: 54.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,49 +1,52 @@
 import java.util.*;
 class Solution {
-    static int answer = Integer.MAX_VALUE;
-    static int r = 0;
-    static int c = 0;
-    static int [] dr = {0, -1, 1, 0}; // 왼 위 아래 오른
-    static int [] dc = {-1, 0, 0, 1}; // 왼 위 아래 오른
-	public int solution(int[][] maps) {
-        r = maps.length;
-        c = maps[0].length;        
-        BFS(maps);
+    static int R, C;
+    static int [] posR = {0, -1, 0, 1}; //왼 위 오 아
+    static int [] posC = {-1, 0, 1, 0}; //왼 위 오 아
+    static boolean [][] visited;
 
-        if(answer == Integer.MAX_VALUE) return -1;
-		return answer;
+    
+	public int solution(int[][] maps) {
+        R = maps.length;
+        C = maps[0].length;
+        
+        //BFS
+        Queue<Pos> q = new ArrayDeque<>();
+        q.add(new Pos(0, 0, 1));
+
+        visited = new boolean [R][C];
+        visited[0][0] = true;
+        while(!q.isEmpty()){
+            Pos poll = q.poll();
+            
+            //만약 도착했으면 리턴
+            if(poll.r == R-1 && poll.c == C-1) return poll.cnt;
+            
+            for(int i=0; i<4; i++){
+                int newR = poll.r + posR[i];
+                int newC = poll.c + posC[i];
+
+                // 유효성 검사
+                if(newR <0 || newR >= R || newC <0 || newC >=C 
+                   || maps[newR][newC] == 0 || visited[newR][newC]) continue;
+                
+                visited[newR][newC] = true;
+                q.add(new Pos(newR, newC, poll.cnt+1));
+            }
+        }
+		return -1;
 	}
     
-    //BFS
-    public void BFS(int[][]maps){
-        Queue<int[]> q = new ArrayDeque<>();
-        q.add(new int[]{0,0,1});
-        boolean [][] visited = new boolean [r][c];
-        visited[0][0] = true;
+    static class Pos{
+        int r;
+        int c;
+        int cnt;
 
-        while(!q.isEmpty()){
-            int [] now = q.poll();
-            int nowR = now[0];
-            int nowC = now[1];
-            int cost = now[2];
-            
-            // r-1, c-1에 도달하면 종료
-            if(nowR == r-1&& nowC == c-1){
-                answer = Math.min(answer, cost);
-                return;
-            }
-            
-            
-            // 동서남북으로 BFS
-            for(int i=0; i<4; i++){
-                int newR = nowR + dr[i];
-                int newC = nowC + dc[i];
-                if(newR >= 0 && r > newR && newC >=0 && newC <c && !visited[newR][newC]
-                  && maps[newR][newC] == 1){
-                    visited[newR][newC] = true;
-                    q.add(new int[]{newR, newC, cost+1});
-                }
-            }
+        
+        public Pos(int r, int c, int cnt){
+            this.r = r;
+            this.c = c;
+            this.cnt = cnt;
         }
     }
 }
