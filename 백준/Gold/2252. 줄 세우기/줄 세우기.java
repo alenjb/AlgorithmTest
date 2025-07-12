895e55dc67a15c96a49468794bf6ade057d993a9
이정빈 <alenjb@naver.com>
2025-07-12 15:57:10 +0900

    [Gold III] Title: 줄 세우기, Time: 384 ms, Memory: 50472 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,40 +1,57 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); // 사람 수 ( 1~N)
-        int M = Integer.parseInt(st.nextToken()); // 비교회수
-
-        List<Integer> [] arr = new List[N+1];
-        for(int i=1; i<=N; i++) arr[i] = new ArrayList<>();
-        int [] inDegree = new int[N+1];
-
-        for(int i=0; i<M; i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken()); // 큰사람
-            int b = Integer.parseInt(st.nextToken()); // 작은사람
-            arr[a].add(b);
-            inDegree[b]++;
-        }
-
-        Queue<Integer> pq = new ArrayDeque<>();
-        for(int i=1; i<=N; i++){
-            if(inDegree[i] == 0) pq.add(i);
-        }
-
-        while (!pq.isEmpty()){
-            int num = pq.poll();
-            for(int nn : arr[num]){
-                inDegree[nn]--;
-                if(inDegree[nn] == 0) pq.add(nn);
-            }
-            System.out.print(num + " ");
-        }
-    }
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+	
+		
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		List<Integer> [] arr = new ArrayList[N+1];
+		int [] in = new int[N+1];
+		
+		for(int i=0; i<=N; i++) {
+			arr[i] = new ArrayList<>();
+		}
+		
+		for(int tc=0; tc<M; tc++) {
+			st = new StringTokenizer(br.readLine());
+			int first = Integer.parseInt(st.nextToken());	
+			int second = Integer.parseInt(st.nextToken());
+			arr[first].add(second);
+			in[second]++;
+		}
+		
+		Queue<Integer> q = new LinkedList<>();
+		for(int i=1; i<N+1; i++) {
+			if(in[i] == 0) q.add(i);
+		}
+		
+		while(!q.isEmpty()) {
+			int now = q.poll();
+			sb.append(now+" ");
+			for(int num : arr[now]) {
+				in[num]--;
+				if(in[num] == 0) q.add(num);
+			}
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
 }
