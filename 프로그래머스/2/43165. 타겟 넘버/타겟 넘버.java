d40657f56ae8129ccbf08d5b2857bec181484c8f
이정빈 <alenjb@naver.com>
2025-10-04 17:53:06 +0900

    [level 2] Title: 타겟 넘버, Time: 208.31 ms, Memory: 188 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,22 +1,38 @@
 import java.util.*;
 class Solution {
-    static int answer = 0;
-    static int l;
+    static List<String> cmdList = new ArrayList<>();
     public int solution(int[] numbers, int target) {
-        l = numbers.length;
-        BFS(0, numbers, target, 0);
+        int answer = 0;
+        int l = numbers.length;
+
+        // + - 만들 수 있는 순열
+        pri("", l);
+        
+        
+        for(int i=0; i<cmdList.size(); i++){
+            // 계산하기
+            int sum = cal(numbers, cmdList.get(i));
+            if(sum == target) answer++;            
+        }
         return answer;
     }
-    
-    // BFS
-    public void BFS(int sum, int[] numbers, int target, int idx){        
-        if(idx == l){ // 길이가 맞으면
-            if(sum == target) answer++;
-            return;
+    static int cal(int [] numbers, String cmds){
+        int sum = cmds.charAt(0) == '-' ?  -numbers[0] : numbers[0];
+        for(int i=1; i<numbers.length; i++){
+            char c = cmds.charAt(i);
+            if(c == '-') sum -= numbers[i];
+            else sum += numbers[i];
         }
-
-        BFS(sum - numbers[idx], numbers, target, idx+1);
-        BFS(sum + numbers[idx], numbers, target, idx+1);
+        return sum;    
     }
     
+    static void pri(String cmds, int limit){
+        if(cmds.length() == limit) {
+            cmdList.add(cmds);
+            return;
+        }
+        
+        pri(cmds+"-", limit);
+        pri(cmds+"+", limit);
+    }
 }
