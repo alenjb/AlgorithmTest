2c38d907708327a13c7414bae3ce668ca43da41f
이정빈 <alenjb@naver.com>
2025-10-06 19:44:01 +0900

    [Gold III] Title: 게임 개발, Time: 480 ms, Memory: 55944 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -1,50 +1,44 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        List<Integer> []  arr = new List[N+1];
+        for(int i=0; i<=N; i++) arr[i] = new ArrayList();
+        int[] buildTime = new int[N+1];
+        int [] totalTime = new int[N+1];
 
-        int[] in = new int[N]; // 진입 차수
-        int[] times = new int[N]; // 기본 건설 시간
-        int[] answers = new int[N]; // 누적 건설 시간
+        int [] inDegree = new int[N+1];
 
-        List<Integer>[] arr = new ArrayList[N]; // 후속 건물 리스트
-        for (int i = 0; i < N; i++) arr[i] = new ArrayList<>();
-
-        for (int tc = 0; tc < N; tc++) {
-            int time = sc.nextInt();
-            times[tc] = time;
-
-            while (true) {
-                int num = sc.nextInt();
-                if (num == -1) break;
-
-                in[tc]++;
-                arr[num - 1].add(tc);
-            }
-        }
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i = 0; i < N; i++) {
-            if (in[i] == 0) {
-                pq.add(i);
-                answers[i] = times[i]; // 초기값 설정
-            }
-        }
-
-        while (!pq.isEmpty()) {
-            int now = pq.poll();
-
-            for (int next : arr[now]) {
-                in[next]--;
-                answers[next] = Math.max(answers[next], answers[now] + times[next]);
-                if (in[next] == 0) {
-                    pq.add(next);
+        for(int i=1; i<=N; i++){
+            buildTime[i] = sc.nextInt();
+            while (true){
+                int prev = sc.nextInt();
+                if(prev == -1) break;
+                else{
+                    arr[prev].add(i);
+                    inDegree[i]++;
                 }
             }
         }
 
-        for (int ans : answers) System.out.println(ans);
+        Queue<Integer> q = new ArrayDeque<>();
+        for(int i=1; i<=N; i++){
+            totalTime[i] = buildTime[i];
+            if(inDegree[i] == 0) q.add(i);
+        }
+        while (!q.isEmpty()){
+            int poll = q.poll();
+            for(int next : arr[poll]){
+                inDegree[next]--;
+                totalTime[next] = Math.max(totalTime[next], totalTime[poll] + buildTime[next]);
+                if(inDegree[next] == 0) q.add(next);
+            }
+        }
+        for(int i=1; i<=N; i++){
+            System.out.println(totalTime[i]);
+        }
     }
 }
