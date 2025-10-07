3a2e92afd75250ed3eb7ff3a50b99e6b828ffc77
이정빈 <alenjb@naver.com>
2025-10-07 17:45:37 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.12 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,55 +1,40 @@
 import java.util.*;
-
 class Solution {
-    static int cnt = 1000000;
-    static List<Integer>[] arr;
-
+    static int [] parent;
     public int solution(int n, int[][] wires) {
-        arr = new ArrayList[n + 1]; // 제네릭 배열 생성
-        // 초기화 수정: `arr[i] = new ArrayList<>();`는 1부터 n까지 초기화 필요
-        for (int i = 1; i <= n; i++) {
-            arr[i] = new ArrayList<>();
-        }
-
-        // 그래프 연결
-        for (int i = 0; i < wires.length; i++) {
-            int a = wires[i][0];
-            int b = wires[i][1];
-            arr[a].add(b);
-            arr[b].add(a);
-        }
-
-        // 간선 하나씩 끊어가며 확인
-        for (int i = 0; i < wires.length; i++) {
-            int count = 0;
-            int a = wires[i][0];
-            int b = wires[i][1];
-
-            // 간선 제거
-            arr[a].remove(Integer.valueOf(b));
-            arr[b].remove(Integer.valueOf(a));
-
-            // BFS 수행
-            Queue<Integer> q = new ArrayDeque<>();
-            boolean[] visited = new boolean[n + 1];
-            q.add(1);
-            while (!q.isEmpty()) {
-                int poll = q.poll();
-                if (visited[poll]) continue; // 방문 여부 확인
-                visited[poll] = true;
-                count++;
-                for (int num : arr[poll]) {
-                    if (!visited[num]) q.add(num);
-                }
+        parent = new int [n+1];
+        int answer = Integer.MAX_VALUE;
+        // 모든 간선에 대해서
+        for(int line = 0; line<n; line++){
+            // 부모배열 초기화
+            for(int i=1; i<=n; i++) parent[i] = i;
+            for(int j=0; j<n-1 ;j++){
+                if(line == j) continue;
+                union(wires[j][0], wires[j][1]);
             }
-
-            // 최소 차이 갱신
-            cnt = Math.min(cnt, Math.abs(count - (n - count)));
-
-            // 간선 복원
-            arr[a].add(b);
-            arr[b].add(a);
+            // 모든 부모에 대해서 find
+            int p = find(1);
+            int num1 = 1;
+            for(int j=2; j<=n; j++){
+                if(find(j) == p) num1++;
+            }
+            int num2 = n - num1;
+            answer = Math.min(answer, Math.abs(num2-num1));
+        }        
+        return answer;
+    }
+    static int find(int num){
+        if(parent[num] == num) return num;
+        return parent[num] = find(parent[num]);
+    }
+    
+    static void union(int a, int b){
+        int aa = find(a);
+        int bb = find(b);
+        
+        if(aa!=bb){
+            parent[aa] = bb;
         }
-        return cnt;
+        
     }
 }
