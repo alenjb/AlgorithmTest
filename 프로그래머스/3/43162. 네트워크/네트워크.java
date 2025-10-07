c067a2d65e0b60877cc383edbc7a5ac7b0897c5f
이정빈 <alenjb@naver.com>
2025-10-07 13:48:03 +0900

    [level 3] Title: 네트워크, Time: 0.78 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,39 +2,33 @@ import java.util.*;
 class Solution {
     static int [] parent;
     public int solution(int n, int[][] computers) {
-        // 부모 초기화
         parent = new int [n];
         for(int i=0; i<n; i++) parent[i] = i;
-        
-        int r = computers.length;
-        int c = computers[0].length;
-        
-        // 입력값으로 유니온
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
                 if(i!=j && computers[i][j] == 1){
                     union(i, j);
                 }
             }
         }
         
-        //parent의 값들 셋에 넣기
-        Set<Integer> set = new HashSet<>();
-        for(int num : parent) set.add(find(num));
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i<n; i++){
+            set.add(find(i));
+        }
         return set.size();
-        
-        
     }
-    
-    public void union(int a, int b){
+    static void union(int a, int b){
         int aa = find(a);
         int bb = find(b);
-        if(aa!= bb) parent[aa] = bb;
+        
+        if(aa!=bb){
+            parent[aa] = bb;
+        }
     }
     
-    public int find(int a){
-        int p = parent[a];
-        if(p == a) return a;
-        return parent[a] = find(p);
+    static int find(int num){
+        if(parent[num] == num) return num;
+        return parent[num] = find(parent[num]);
     }
 }
