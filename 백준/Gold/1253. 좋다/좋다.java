7a58179dccfc546587355096d0cb1b1bd2712d58
이정빈 <alenjb@naver.com>
2025-10-15 22:51:33 +0900

    [Gold IV] Title: 좋다, Time: 240 ms, Memory: 18800 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,36 +1,41 @@
 import java.util.*;
 public class Main{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int [] arr = new int [n];
-        for(int i=0; i<n; i++) arr[i] = sc.nextInt();
+        int num = sc.nextInt();
+        int [] arr = new int [num];
+        for(int i=0;i<num;i++){
+            int n1 = sc.nextInt();
+            arr[i] = n1;
+        }
         Arrays.sort(arr);
-
         int answer = 0;
 
-        // 가능한 조합 set
-        for(int w=0; w<n; w++){
-            int want = arr[w];
-            int i = 0;
-            int j = n-1;
-            while (i < j){
-                if(i == w){
-                    i++;
-                    continue;
-                }
-                else if(j == w){
-                    j--;
-                    continue;
-                }
-                int now = arr[i] + arr[j];
-                if(now == want){
-                    answer++;
-                    break;
-                }else if(now < want) i++;
-                else j--;
+        // 각각 대상이 되며
+        for(int i=0; i<num; i++){
+            int left = 0;
+            int right = num-1;
+
+            while(left <right){
+                int lN = arr[left];
+                int rN = arr[right];
+                int sum = lN + rN;
+                if(sum == arr[i]){
+                    if(left == i){
+                        left++;
+                    }else if(right == i){
+                        right--;
+                    }else {
+                        answer++;
+                        break;
+                    }
+
+                }else if(sum < arr[i]){
+                    left++;
+                }else right--;
             }
         }
         System.out.println(answer);
     }
+
 }
