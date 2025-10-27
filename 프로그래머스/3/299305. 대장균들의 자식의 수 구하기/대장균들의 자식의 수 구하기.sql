9e38150a8fe0a7efa8e841bbf0818a87ab32e126
이정빈 <alenjb@naver.com>
2025-10-27 22:24:02 +0900

    [level 3] Title: 대장균들의 자식의 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
+++ 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
@@ -1,8 +1,26 @@
-# 대장균 개체의 ID(ID)와 자식의 수(CHILD_COUNT)를 출력하는 SQL 문을 작성해주세요. 자식이 없다면 자식의 수는 0으로 출력해주세요
+SELECT 
+    A.ID,
+    IFNULL((
+        SELECT COUNT(*)
+        FROM ECOLI_DATA B
+        WHERE B.PARENT_ID = A.ID
+    ), 0) AS CHILD_COUNT
+FROM ECOLI_DATA A;
 
-# PARENT_ID를 기준으로 group으로 묶기
-# cnt로 해서 출력
-SELECT A.ID, IFNULL((select COUNT(B.PARENT_ID) FROM ECOLI_DATA B WHERE A.ID = B.PARENT_ID GROUP BY B.PARENT_ID),0) AS CHILD_COUNT
-# , 0)
-FROM ECOLI_DATA A
 
+
+
+
+
+
+
+
+
+
+
+# SELECT A.ID, IFNULL(
+#     (SELECT COUNT(B.PARENT_ID) 
+#      FROM ECOLI_DATA B 
+#      WHERE A.ID = B.PARENT_ID 
+#      GROUP BY B.PARENT_ID),0) AS CHILD_COUNT
+# FROM ECOLI_DATA A
