7860c5ee30afbcd0b8c13140c95ff73701036561
이정빈 <alenjb@naver.com>
2025-10-27 21:26:00 +0900

    [level 2] Title: 중복 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
+++ 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
@@ -1,5 +1,18 @@
-select count(distinct name) as 'count'
-from ANIMAL_INS
+select count(distinct NAME) as 'count' from ANIMAL_INS
+
+
+
+
+
+
+
+
+
+
+
+
+# select count(distinct name) as 'count'
+# from ANIMAL_INS
 
 -- 코드를 입력하세요
 # SELECT count (DISTINCT NAME) AS 'count'
