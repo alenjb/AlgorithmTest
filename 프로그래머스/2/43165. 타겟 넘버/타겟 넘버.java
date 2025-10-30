3c1df91bae1ee1be52f449b3be50121cb121ac57
이정빈 <alenjb@naver.com>
2025-10-30 22:20:33 +0900

    [level 2] Title: 타겟 넘버, Time: 5.76 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,38 +1,19 @@
 import java.util.*;
 class Solution {
-    static List<String> cmdList = new ArrayList<>();
+    static public int ans = 0;
     public int solution(int[] numbers, int target) {
-        int answer = 0;
         int l = numbers.length;
-
-        // + - 만들 수 있는 순열
-        pri("", l);
-        
-        
-        for(int i=0; i<cmdList.size(); i++){
-            // 계산하기
-            int sum = cal(numbers, cmdList.get(i));
-            if(sum == target) answer++;            
-        }
-        return answer;
+        DFS(numbers, target, 0, 0);
+        return ans;
     }
-    static int cal(int [] numbers, String cmds){
-        int sum = cmds.charAt(0) == '-' ?  -numbers[0] : numbers[0];
-        for(int i=1; i<numbers.length; i++){
-            char c = cmds.charAt(i);
-            if(c == '-') sum -= numbers[i];
-            else sum += numbers[i];
-        }
-        return sum;    
-    }
-    
-    static void pri(String cmds, int limit){
-        if(cmds.length() == limit) {
-            cmdList.add(cmds);
+    static void DFS(int [] numbers, int target, int idx, int sum){
+        if(idx == numbers.length){
+            if(sum == target) ans++;
             return;
         }
-        
-        pri(cmds+"-", limit);
-        pri(cmds+"+", limit);
+        // 덧셈
+        DFS(numbers, target, idx+1, sum + numbers[idx]);
+        // 뺄셈
+        DFS(numbers, target, idx+1, sum - numbers[idx]);
     }
 }
