f23be03bbcef6b247846c6d686f4eaa8c7f6f517
이정빈 <alenjb@naver.com>
2025-10-31 21:56:45 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.34 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,40 +1,53 @@
-import java.util.*;
+import java.util.*; 
 class Solution {
     static int [] parent;
     public int solution(int n, int[][] wires) {
         parent = new int [n+1];
+        for(int i=1; i<=n; i++) parent[i] = i;
+        
+        for(int i=0; i<wires.length; i++){
+            union(wires[i][0], wires[i][1]);
+        }
+        
         int answer = Integer.MAX_VALUE;
-        // 모든 간선에 대해서
-        for(int line = 0; line<n; line++){
-            // 부모배열 초기화
-            for(int i=1; i<=n; i++) parent[i] = i;
-            for(int j=0; j<n-1 ;j++){
-                if(line == j) continue;
-                union(wires[j][0], wires[j][1]);
-            }
-            // 모든 부모에 대해서 find
-            int p = find(1);
-            int num1 = 1;
-            for(int j=2; j<=n; j++){
-                if(find(j) == p) num1++;
-            }
-            int num2 = n - num1;
-            answer = Math.min(answer, Math.abs(num2-num1));
-        }        
-        return answer;
+        for (int i = 0; i < wires.length; i++) {
+    // 1️⃣ parent 새로 초기화
+    for (int j = 1; j <= n; j++) parent[j] = j;
+    
+    // 2️⃣ i번째 간선 제외하고 연결
+    for (int j = 0; j < wires.length; j++) {
+        if (i == j) continue;
+        union(wires[j][0], wires[j][1]);
     }
-    static int find(int num){
-        if(parent[num] == num) return num;
-        return parent[num] = find(parent[num]);
+
+    // 3️⃣ 그룹별 노드 개수 계산
+    int[] cnt = new int[n+1];
+    for (int j = 1; j <= n; j++) {
+        cnt[find(j)]++;
+    }
+
+    int max = 0, min = 101;
+    for (int j = 1; j <= n; j++) {
+        if (cnt[j] > 0) {
+            max = Math.max(max, cnt[j]);
+            min = Math.min(min, cnt[j]);
+        }
+    }
+
+    answer = Math.min(answer, max - min);
+}
+
+        return answer;
     }
     
     static void union(int a, int b){
         int aa = find(a);
         int bb = find(b);
-        
-        if(aa!=bb){
-            parent[aa] = bb;
-        }
-        
+        parent[aa] = bb;
+    }
+    
+    static int find(int num){
+        if(parent[num] == num) return num;
+        return parent[num] = find(parent[num]);
     }
 }
