830fe8a623caeaad88162debc91bf7f54fecc7c1
이정빈 <alenjb@naver.com>
2026-04-06 22:23:17 +0900

    [level 2] Title: 전화번호 목록, Time: 341.36 ms, Memory: 99.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,8 +1,9 @@
 import java.util.*;
 class Solution {
     public boolean solution(String[] pb) {
+        int n = pb.length;
         Arrays.sort(pb);
-        for(int i=0; i<pb.length-1; i++){
+        for(int i=0; i<n-1; i++){
             if(pb[i+1].startsWith(pb[i])) return false;
         }
         return true;
