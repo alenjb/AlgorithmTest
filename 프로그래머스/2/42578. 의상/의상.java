c3a223d9e68ceddee35bf34313461748eea0fd71
이정빈 <alenjb@naver.com>
2026-04-06 22:47:06 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,18 +1,18 @@
 import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
-        HashMap<String, Integer> map = new HashMap<>();
-        for(int i=0; i<clothes.length; i++){
-            String type = clothes[i][1];
-            if(map.get(type) != null){
-                int now = map.get(type);
-                map.put(type, now+1);
-            }else map.put(type, 1);
+        int n = clothes.length;
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<n ;i++){
+            String s =  clothes[i][0];
+            String kind = clothes[i][1];
+            map.put(kind, map.getOrDefault(kind, 0)+1);
         }
-        int answer = 1;
+        int sum = 1;
         for(int num : map.values()){
-            answer *= (num+1);
+            sum = sum * (num +1);
         }
-        return answer-1;
+        sum--;
+        return sum;
     }
 }
