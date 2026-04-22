44620a364121871440e018b6935874ad22d01465
이정빈 <alenjb@naver.com>
2026-04-22 22:17:22 +0900

    [level 2] Title: H-Index, Time: 0.88 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,29 +1,18 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int[] citations) {
-        int left = 0;
-        int right = citations.length;
-        int answer = 0;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (isValid(mid, citations)) {
-                answer = mid;     // 조건 만족: 더 큰 h 가능성 확인
-                left = mid + 1;
-            } else {
-                right = mid - 1;  // 조건 불만족: h를 줄여야 함
+    public int solution(int[] cit) {
+        Arrays.sort(cit);
+        int n = cit.length;
+        
+        for (int i = 0; i < n; i++) {
+            int h = n - i; // 이 위치에서 가능한 H값
+            
+            if (cit[i] >= h) {
+                return h;
             }
         }
-
-        return answer;
-    }
-
-    private boolean isValid(int h, int[] citations) {
-        int count = 0;
-        for (int c : citations) {
-            if (c >= h) count++;
-        }
-        return count >= h;
+        
+        return 0;
     }
 }
