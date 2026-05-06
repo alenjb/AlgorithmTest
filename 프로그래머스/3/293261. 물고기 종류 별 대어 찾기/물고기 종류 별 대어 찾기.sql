b05e0f5a6da6a49177730f809dac2f2219f1d2cc
이정빈 <alenjb@naver.com>
2026-05-06 22:23:47 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,14 +1,35 @@
-# 물고기 종류별로 가장 큰 물고기의 id, 이름 길이
-select c.id, d.fish_name, c.length
-from fish_name_info d
-join
-(
-    SELECT A.LENGTH, A.Id, a.fish_type
-    FROM FISH_INFO A
-    JOIN 
-    (
-        SELECT FISH_TYPE, MAX(LENGTH) as length
-        FROM FISH_INFO
-        GROUP BY FISH_TYPE
-    ) B ON A.LENGTH = B.LENGTH and a.fish_type = b.fish_type
-) c on d.fish_type = c.fish_type
+SELECT C.ID, D.FISH_NAME, D.LENGTH FROM FISH_INFO C JOIN 
+(SELECT B.FISH_NAME, MAX(A.LENGTH) AS LENGTH, A.FISH_TYPE FROM FISH_INFO A JOIN FISH_NAME_INFO B ON A.FISH_TYPE = B.FISH_TYPE
+GROUP BY A.FISH_TYPE) D
+ON C.LENGTH = D.LENGTH AND C.FISH_TYPE = D.FISH_TYPE
+ORDER BY C.ID
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # 물고기 종류별로 가장 큰 물고기의 id, 이름 길이
+# select c.id, d.fish_name, c.length
+# from fish_name_info d
+# join
+# (
+#     SELECT A.LENGTH, A.Id, a.fish_type
+#     FROM FISH_INFO A
+#     JOIN 
+#     (
+#         SELECT FISH_TYPE, MAX(LENGTH) as length
+#         FROM FISH_INFO
+#         GROUP BY FISH_TYPE
+#     ) B ON A.LENGTH = B.LENGTH and a.fish_type = b.fish_type
+# ) c on d.fish_type = c.fish_type
