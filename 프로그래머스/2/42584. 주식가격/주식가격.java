fef90aa2d739d44138e9cf6f9da24ddc5978bdd3
이정빈 <alenjb@naver.com>
2026-05-16 21:17:08 +0900

    [level 2] Title: 주식가격, Time: 33.44 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,24 +1,28 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(int[] prices) {
         int n = prices.length;
-        int[] answer = new int[n];
+        int[] answer = new int [n];
         Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
-                int idx = stack.pop();
-                answer[idx] = i - idx;
+        stack.push(0);
+        for(int i=1; i<n; i++){
+            if(stack.isEmpty()) stack.push(i);
+            else{
+                int peek = stack.peek();
+                if(prices[peek] > prices[i]){ // 떨어졌으면
+                    while(!stack.isEmpty() && prices[stack.peek()] > prices[i]){// 똑같거나 큰거 찾을 때까지 빼기 
+                        int pop = stack.pop();
+                        answer[pop] = i - pop;
+                    }
+                }
             }
             stack.push(i);
-        }
 
-        while (!stack.isEmpty()) {
-            int idx = stack.pop();
-            answer[idx] = n - 1 - idx;
         }
-
+        while(!stack.isEmpty()){
+            int pop = stack.pop();
+            answer[pop] = n - 1 - pop;
+        }
         return answer;
     }
 }
