62a2ced419dc955ba4f2f12653567f6295329fbf
이정빈 <alenjb@naver.com>
2026-05-16 20:48:53 +0900

    [level 2] Title: 올바른 괄호, Time: 8.82 ms, Memory: 57.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,19 +1,21 @@
 import java.util.*;
 class Solution {
     boolean solution(String s) {
+        boolean answer = true;
         Stack<Character> st = new Stack<>();
-        int n = s.length();
-        for(int i=0; i<n; i++){
+        for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
-            if(c == '(') st.add(c);
+            // (가 들어오면 넣기
+            if(c == '(') st.push(c);
             else{
-                if(st.isEmpty()) return false;
-                else if(st.peek() == ')') return false;
-                else st.pop();
+            // )가 들어오면 빼기
+                // 뺄게 없으면 false OR )가 나오면 false;
+                if(st.isEmpty() || st.peek() == ')') return false;
+                // (가 나오면 빼고 넘어가기
+                else if(st.peek() == '(') st.pop();
             }
-            
         }
         if(!st.isEmpty()) return false;
-        return true;
+        return answer;
     }
 }
