c91c59dc699850ab0f8030581eb09ea530211214
이정빈 <alenjb@naver.com>
2026-05-16 22:19:49 +0900

    [level 3] Title: 단어 변환, Time: 0.33 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,46 +1,60 @@
 import java.util.*;
-
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        if (!Arrays.asList(words).contains(target)) return 0;
+        int n = words.length + 2;
+        String [] words2 = new String [n];
+        for(int i=0; i<n-2; i++){
+             words2[i] = words[i];
+        }
+        words2[n-2] = begin;
+        words2[n-1] = target;
+        boolean [] visited = new boolean[n];
+        int [] dist = new int [n];
 
-        Queue<Node> q = new ArrayDeque<>();
-        boolean[] visited = new boolean[words.length];
-        q.add(new Node(begin, 0));
-
-        while (!q.isEmpty()) {
-            Node poll = q.poll();
-            String now = poll.word;
-            int count = poll.cnt;
-
-            if (now.equals(target)) return count;
-
-            for (int i = 0; i < words.length; i++) {
-                if (!visited[i] && canConvert(now, words[i])) {
-                    visited[i] = true;
-                    q.add(new Node(words[i], count + 1));
+        //BFS
+        List<Integer> [] arr = new List[n];
+        for(int i=0; i<n; i++) arr[i] = new ArrayList<>();
+        
+        for(int i=0 ;i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i == j) continue;
+                if(isOneDiff(words2[i], words2[j])) {
+                    arr[i].add(j);
+                    arr[j].add(i);
+                }
+            }
+        }
+        
+        int bIdx = n-2;
+        int tIdx = n-1;
+        
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(bIdx);
+        visited[bIdx] = true;
+        int maxCount = 0;
+        while(!q.isEmpty()){
+            int poll = q.poll();
+            for(int next : arr[poll]){
+                if(!visited[next]){
+                    visited[next] = true;
+                    dist[next] = dist[poll] + 1;
+                    if(words2[next].equals(target) && next!= tIdx) return dist[next];
+                    q.add(next);
                 }
             }
         }
         return 0;
     }
-
-    static boolean canConvert(String a, String b) {
+    public boolean isOneDiff(String a, String b){
+        int n = a.length();
         int diff = 0;
-        for (int i = 0; i < a.length(); i++) {
-            if (a.charAt(i) != b.charAt(i)) diff++;
-            if (diff > 1) return false;
+        for(int i=0; i<n; i++){
+            char c1 = a.charAt(i);
+            char c2 = b.charAt(i);
+            if(c1 != c2) diff++;
+            if(diff >1) return false;
         }
-        return diff == 1;
-    }
-
-    static class Node {
-        String word;
-        int cnt;
-
-        Node(String word, int cnt) {
-            this.word = word;
-            this.cnt = cnt;
-        }
+        if(diff == 1) return true;
+        return false;
     }
 }
