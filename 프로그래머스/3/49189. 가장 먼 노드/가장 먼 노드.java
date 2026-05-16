f14234e1580e85c5a477142f6284d603a1812800
이정빈 <alenjb@naver.com>
2026-05-16 21:44:49 +0900

    [level 3] Title: 가장 먼 노드, Time: 46.43 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,56 +1,41 @@
 import java.util.*;
 class Solution {
-    //BFS
     public int solution(int n, int[][] edge) {
-
-        int r = edge.length;
-        int c = edge[0].length;
-    
         List<Integer> [] arr = new List[n+1];
+        for(int i=0; i<n+1; i++) arr[i] = new ArrayList<>();
         boolean [] visited = new boolean [n+1];
-        for(int i=1; i<=n; i++) arr[i] = new ArrayList<>();
-        for(int i=0; i<r; i++){
+        int [] dist = new int [n+1];
+        
+        for(int i=0; i<edge.length; i++){
             int a = edge[i][0];
             int b = edge[i][1];
             arr[a].add(b);
             arr[b].add(a);
         }
         
-        int max = 0;
-        Set<Integer> set = new HashSet<>();
-        
-        //bfs
-        Queue<Node> q = new ArrayDeque<>();
-        q.add(new Node(1, 0));
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(1);
         visited[1] = true;
-        
+        int depth = 0;
         while(!q.isEmpty()){
-            Node poll = q.poll();
-            if(poll.cnt > max){
-                set = new HashSet<>();
-                set.add(poll.num);
-                max = poll.cnt;
-            }else if(poll.cnt == max){
-                set.add(poll.num);   
-            }
-                
-            for(int next : arr[poll.num]){
+            int now = q.poll();
+            for(int next : arr[now]){
                 if(!visited[next]) {
-                    q.add(new Node(next, poll.cnt+1));
+                    q.add(next);
+                    dist[next] = dist[now]+1;
                     visited[next] = true;
                 }
             }
         }
-        return set.size();
-    }
-    
-    static class Node{
-        int num;
-        int cnt;
-        
-        public Node(int n, int c){
-            num = n;
-            cnt = c;
+        for(int nn : dist){
+            System.out.println(nn);
         }
+        int maxDepth = 0;
+        for(int i=0; i<n+1; i++) maxDepth = Math.max(maxDepth, dist[i]);
+        int answer = 0;
+        for(int i=0; i<n+1; i++) {
+            if(maxDepth == dist[i]) answer++;
+        }
+        return answer;
     }
 }
