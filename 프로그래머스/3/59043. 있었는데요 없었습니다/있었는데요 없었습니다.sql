dbb2d816115acddd873c4c72723c2f5210750c89
이정빈 <alenjb@naver.com>
2026-05-17 22:27:00 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,27 +1,6 @@
-SELECT A.ANIMAL_ID, A.NAME
-FROM ANIMAL_INS A JOIN ANIMAL_OUTS B ON A.ANIMAL_ID = B.ANIMAL_ID
-WHERE A.DATETIME > B.DATETIME
-ORDER BY A.DATETIME
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# -- 코드를 입력하세요
-# SELECT a.ANIMAL_ID,	a.NAME
-# from ANIMAL_INS a join ANIMAL_OUTS b  on a.ANIMAL_ID = b.ANIMAL_ID
-# where DATEDIFF(b.DATETIME, a.DATETIME) < 0
-# order by a.DATETIME
+# 보호 시작일보다 입양일이 더 빠른 동물의 아이디와 이름
+# 결과는 보호 시작일이 빠른 순
+select a.animal_id, a.name
+from animal_ins a join animal_outs b on a.animal_id = b.animal_id
+where a.datetime > b.datetime
+order by a.datetime asc
